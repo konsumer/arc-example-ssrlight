142bc8c1c580d93e3a7d93f9997e143a680c0870
David Konsumer <konsumer@jetboystudio.com>
2020-02-14 15:46:40 -0800

    friendlier startup

--- src/dev.js
+++ src/dev.js
@@ -4,6 +4,7 @@ import Bundler from 'parcel-bundler'
 import express from 'express'
 import sandbox from '@architect/sandbox'
 import proxy from 'http-proxy-middleware'
+import chalk from 'chalk'
 
 const app = express()
 const { PORT = 8080 } = process.env
@@ -12,8 +13,8 @@ const bundler = new Bundler(`${__dirname}/frontend/index.html`)
 // parcel does this too, but this ensures that other HTML files work correctly
 app.use(express.static(`${__dirname}/frontend/static`))
 app.use(bundler.middleware())
-app.use(proxy({ target: `http://0.0.0.0:${PORT + 1}`, changeOrigin: true }))
+app.use(proxy({ target: `http://0.0.0.0:${PORT + 1}`, changeOrigin: true, logLevel: 'silent' }))
 
-console.log(`Dev server listening on http://0.0.0.0:${PORT}`)
+console.log(`\nDev server listening on ${chalk.blue(`http://0.0.0.0:${PORT}`)}\n\n`)
 sandbox.start({ port: PORT + 1, quiet: true })
 app.listen(PORT)
