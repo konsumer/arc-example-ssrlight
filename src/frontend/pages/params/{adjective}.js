fff1b43a8d88382e57e84a06b6099f75aa721553
David Konsumer <konsumer@jetboystudio.com>
2020-02-14 11:46:11 -0800

    hmm, don;t really need to support params

--- src/frontend/pages/params/{adjective}.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default ({ adjective = 'None' }) => (
-  <main>
-    <h2>Params</h2>
-    <p>This shows how to use URL params. For example, you used "{adjective}".</p>
-  </main>
-)
