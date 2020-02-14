c1e7ff078240b8d33624feb16d854783b143a91a
David Konsumer <konsumer@jetboystudio.com>
2020-02-14 03:35:18 -0800

    reuse pages in router

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -6,11 +6,11 @@ import { useRoutes } from 'hookrouter'
 
 import './style.css'
 import pages from './pages/**/*.js'
-import NotFoundPage from './pages/404'
-import IndexPage from './pages/index'
 
 // this builds the router from files in pages/*.js
 const Router = () => {
+  const IndexPage = pages.index
+  const NotFoundPage = pages['404']
   const routes = { '/': () => <IndexPage /> }
   Object.keys(pages).forEach(p => {
     if (p !== 'index' && p !== '404') {
